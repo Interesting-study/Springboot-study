f2b4a3e15fb8e3bdf16a1d73d111f90b60bae474
Slowth-KIM <rt.slowth@gmail.com>
2021-02-16 20:46:06 +0900

    엔티티 설계 1

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+// Getter는 열어두지만, Setter는 잘 생각해봐야함
+public class Address {
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
