ecabfdaa602153162ef2d99778e9fc99673d7cfa
Slowth-KIM <rt.slowth@gmail.com>
2021-03-17 11:05:25 +0900

    app config 설정 및 구체적 구현 필요

--- core/src/main/java/hello/core/order/OrderSerivceImpl.java
+++ core/src/main/java/hello/core/order/OrderSerivceImpl.java
@@ -1,15 +1,20 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderSerivceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private MemberRepository memberRepository;
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy;
+
+    public OrderSerivceImpl(MemoryMemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
