4f374aeb8ae1e58d4a9fedec3fc8d13aadc2db67
Slowth-KIM <rt.slowth@gmail.com>
2021-04-22 11:14:11 +0900

    빈 생명주기 콜백

---
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,47 @@
+package hello.core.lifecycle;
+
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient{
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    //서비스 시작
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url +"message = " + message);
+    }
+
+    public void disconnect(){
+        System.out.println("cloese:" + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Networkclient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("Networkclient.close");
+        disconnect();
+    }
+}
