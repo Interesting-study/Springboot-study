684af9c5eb5026e55f9b55b2abf89093bb0e3105
Slowth-KIM <rt.slowth@gmail.com>
2021-05-30 21:54:19 +0900

    웹 계층 개발 완료

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,11 +5,11 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.*;
 import java.util.List;
 
 @Service
@@ -58,9 +58,8 @@ public class OrderService {
         order.cancel();
     }
 
-    //검색
-    /*
+
     public List<Order> findOrders(OrderSearch orderSearch){
-        return orderRepository.findAll(orderSearch);
-    }*/
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
